c8eb09ba49505648ad2a459470088f1bcc92fd52
habibu <habib.az@hotmail.com>
2015-12-22 19:28:08 -0200

    Chaging enroller to Enrolled
    Spanish to English comments

--- enrolar.py
+++ enrolar.py
@@ -3,6 +3,7 @@ Created on 08/04/2014
 
 @author: jeanmachuca
 '''
+#Here the English file name should be Enrolled, I think he used Spanish cos of he created a function with named Erroll
 import FPS, sys
 from test_raw import *
 
